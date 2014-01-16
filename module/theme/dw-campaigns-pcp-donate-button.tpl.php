bf58fe0a8d58756e92b73aa92b11249c247812de
Naylor <jNaylor@o2oInteractive.com>
2014-01-16 08:39:02 -0700

    offline form mods.

--- module/theme/dw-campaigns-pcp-donate-button.tpl.php
+++ module/theme/dw-campaigns-pcp-donate-button.tpl.php
@@ -15,9 +15,20 @@ if(!is_null($document)) {
     <div class="menu-item-word-outter"><div class="menu-item-word-inner"><?php echo $donate_text; ?></div></div>
 </a>
 
-<?php 
-if(!is_null($document)) { 
-?>
-<div class="donation-form"><a href="<?php echo $document; ?>"><?php echo t('If you are unable to donate online, please print out a donation form.');?></a></div>
-<?php
-}
+<?php if(!is_null($document)) { ?>
+    <div class="donation-form"><a href="<?php echo $document; ?>"><?php echo t('If you are unable to donate online, please print out a donation form.');?></a></div>
+
+    <?php   //JFN - january 15 2014 1826 - [#feature "Select and upload a pdf to replace the currently linked document here.  but only if you're logged in as an admin"]
+
+        if( in_array('site admin', array_values($user->roles)) ) {
+            $myForm = drupal_get_form('dw_campaigns_upload_offline_form_form', $document );
+
+            print drupal_render( $myForm );
+            //drupal_render( $myForm );
+
+            //drupal_render();
+        //    echo '<div class="admin-link"><a href=""> admin->[replace donation form] </a></div>';
+        }
+
+    ?>
+<?php } ?>
