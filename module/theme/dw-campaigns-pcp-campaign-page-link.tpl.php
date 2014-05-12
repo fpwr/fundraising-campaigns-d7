b062265b4da493c35d08636b1989478cf9395ee8
ryanrd <ryan@refreshdigital.com>
2014-05-12 09:05:53 -0500

    change for api3 credit card error messages, pcp campaign link error fix

--- module/theme/dw-campaigns-pcp-campaign-page-link.tpl.php
+++ module/theme/dw-campaigns-pcp-campaign-page-link.tpl.php
@@ -1,1 +1,11 @@
+<?php 
+if(isset($campaign->nid) && isset($campaign->title)) {
+?>
+
 <a href="/dw/walking/location/<?php echo $campaign->nid;?>">Return to <?php echo $campaign->title;?></a>
+
+<?php
+} else {
+watchdog('hmm, no campaign', $_SERVER['REQUEST_URI']);
+}
+?>
